db746bb4660bdb7a63e5dc8e3ea6280fabe885ea
GustavoMarrufo <gustavitotlgac@gmail.com>
2025-06-13 20:07:08 -0600

    Ejercicio 1 - If-Else

---
+++ C#/Exercises/RP1-NB/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace RetoProgramación1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escribe un número entero");
+            int numero = NumeroIngresado();
+
+            if (numero > 10)
+            {
+                Console.WriteLine("El número es mayor a 10");
+            }
+            else
+            {
+                Console.WriteLine("El número no es mayor a 10");
+            }
+
+        }
+
+        static int NumeroIngresado()
+        {
+            int dato;
+            bool resultado;
+
+            do
+            {
+                string entrada = Console.ReadLine() ?? "";
+                resultado = int.TryParse(entrada, out dato);
+                if (!resultado)
+                {
+                    Console.WriteLine("El número ingresado es inválido, porfavor ingresar otro");
+                }
+            } while (!resultado);
+            return dato;
+        }
+    }
+}
+
