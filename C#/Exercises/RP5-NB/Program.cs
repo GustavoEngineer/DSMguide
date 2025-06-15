42167dcbf29089878a9f1a95089774847b4dea09
GustavoMarrufo <gustavitotlgac@gmail.com>
2025-06-15 13:51:42 -0600

    Ejercicio 5 de programación

---
+++ C#/Exercises/RP5-NB/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace RetoProgramacion5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nombres = { "Juan", "Lee", "Jorge", "Uziel", "Erick", "Carlos", "Blanca" };
+
+            foreach (string nombre in nombres)
+            {
+                Console.WriteLine("El nombre es: " + nombre);
+            }
+        }
+    }
+}
